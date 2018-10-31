11eafebdff0d1e95114f49639e79321cb03ea90f
jose montoya <jose_montoya_guzman@hotmail.com>
2018-10-31 15:41:12 -0600

    first commit

---
+++ 2_Tipos de datos/Datos.java
@@ -0,0 +1,27 @@
+class Datos {
+  public static void main(String args[]) {
+
+    // DECLARAR VARIABLES
+    String nombre;
+    int numero;
+    // INICIALIZAR VARIABLE
+    nombre = "montoya";
+    numero = 3;
+
+    // DECLARAR E INICIALIZAR A LA VEZ
+    String nombre2 = "Victor";
+    double numDecimal = 23.20;
+    float numDecimal2 = 250.10f;
+    char letra = 'a';
+    boolean botonEncendido = true;
+
+    System.out.println(nombre);
+    System.out.println(numero);
+    System.out.println(nombre2);
+    System.out.println(numDecimal);
+    System.out.println(numDecimal2);
+    System.out.println(letra);
+    System.out.println(botonEncendido);
+
+  }
+}
