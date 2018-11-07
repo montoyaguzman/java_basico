70b5bcba13e4363af46ba0bfe8cea04d505debdf
jose montoya <jose_montoya_guzman@hotmail.com>
2018-11-07 15:28:07 -0600

    ciclo for

---
+++ 8_Ciclos/CicloFor.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CicloFor {
+  public static void main(String[] args) {
+
+    /*
+     * for (initial, condicion, incremento) {
+     * 
+     * }
+     */
+
+    // Imprimir numeros pares del 0 al 10
+    for (int i = 0; i <= 10; i++) {
+      if (i % 2 == 0) {
+        System.out.println(i);
+      }
+    }
+
+  }
+}
