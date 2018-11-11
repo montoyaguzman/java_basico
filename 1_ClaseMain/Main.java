debef5b05add304a7f2d259e2c563265448cdf95
Jose Montoya Guzman <Administradorcito@MB80034.cb.bbvabancomer.com.mx>
2018-11-11 10:05:37 -0600

    corrige orden de programas

--- 1_ClaseMain/Main.java
+++ 1_ClaseMain/Main.java
@@ -1,11 +1,6 @@
 class Main {
   public static void main(String args[]) {
-    // Crear objeto
-    // Crear instancia
-    Cliente obj1 = new Cliente();
-    Usuario usuario = new Usuario();
-    System.out.println(obj1);
-    System.out.println(usuario instanceof Object);
+    System.out.println("Hola mundo!!");
 
   }
 }
