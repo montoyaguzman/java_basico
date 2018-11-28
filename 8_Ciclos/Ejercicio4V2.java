fdbe72c81ce33e20ba6992953c65c141fb7a2aa9
montoyaguzman <montoyaguzman7@gmail.com>
2018-11-28 12:26:53 -0600

    agrega suma de valores con scanner

---
+++ 8_Ciclos/Ejercicio4V2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ejercicio4V2 {
+  public static void main(String[] args) {
+    int inicio = 25;
+
+    while(inicio <= 50) {
+      System.out.println(inicio);
+      // inicio = inicio + 5;
+      inicio+=5;
+    }
+
+
+
+  }
+}
